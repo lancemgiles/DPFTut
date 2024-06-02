995c3ee05e7b2d711e10044db21f60e0f19202d8
lance <dogmandevices@gmail.com>
2024-06-02 00:15:18 -0500

    add metadata

--- plugins/MyAmp/MyAmp.cpp
+++ plugins/MyAmp/MyAmp.cpp
@@ -6,7 +6,17 @@ class MyAmp : public Plugin {
   public:
     MyAmp() : Plugin(kParameterCount, 0, 0), gain(1.0) {}
 
-    
+  protected:
+    const char *getLabel() const override { return "MyAmp"; }
+    const char *getDescription() const override {
+      return "Simple amp plugin";
+    }
+    const char *getMaker() const override { return "dogman devices"; }
+    const char *getLicense() const override { return "MIT"; }
+    uint32_t getVersion() const override { return d_version(1,0,0); }
+    int64_t getUniqueId() const override {
+      return d_cconst('M','A','D','T'); // My Amp Dpf Tutorial
+    }
 
   private:
     float gain;
